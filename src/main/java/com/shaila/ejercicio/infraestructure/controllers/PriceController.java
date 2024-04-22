8d26751a805440abef98172a53365b847d616579
Shaila <shailaperezfernandez@gmail.com>
2024-04-22 10:56:38 +0200

    Documentación app
    Configuración pom y OpenApiConfig para documentar la app

--- src/main/java/com/shaila/ejercicio/infraestructure/controllers/PriceController.java
+++ src/main/java/com/shaila/ejercicio/infraestructure/controllers/PriceController.java
@@ -3,12 +3,15 @@ package com.shaila.ejercicio.infraestructure.controllers;
 
 import com.shaila.ejercicio.application.services.PriceService;
 import com.shaila.ejercicio.infraestructure.dto.ResponsePriceDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/prices")
+@RequestMapping("app/prices")
+@Tag(name ="Precios resource")
 public class PriceController {
 
     private final PriceService priceService;
@@ -17,6 +20,7 @@ public class PriceController {
         this.priceService = priceService;
     }
 
+    @Operation(summary="Consultar precio final de un producto para una cadena textil en una fecha específica")
     @GetMapping
     public ResponseEntity<ResponsePriceDto> getInfoPrices(
             @RequestParam Long brandId,

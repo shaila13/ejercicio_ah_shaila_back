acbb3ad4d174f14575df9f11721ecc67fd3843df
Shaila <shailaperezfernandez@gmail.com>
2024-04-22 08:26:20 +0200

    Estructura proyecto
    Estructura proyecto y configuracion dependecias

---
+++ src/test/java/com/shaila/ejercicio/EjercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaila.ejercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

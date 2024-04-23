abdb2d899981341669faf6647324ae3daf375d9c
Shaila <shailaperezfernandez@gmail.com>
2024-04-23 08:05:03 +0200

    Readme
    Modificaciones Readme

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 
 # Servicio de Consulta de Precios
-Este proyecto es una aplicación Spring Boot que proporciona un servicio REST para consultar precios en un sistema de comercio electrónico que sigue la arquitectura hexagonal. Utiliza una base de datos en memoria H2 para almacenar y recuperar datos de precios.
-
-Este es un proyecto inicial para Java 17 con Maven.
+Este proyecto es una aplicación Spring Boot que proporciona un servicio REST para consultar precios en un sistema de comercio electrónico. Permite a los usuarios obtener información detallada sobre los precios de productos en diferentes marcas y fechas específicas. Utiliza una base de datos en memoria H2 para almacenar y recuperar los datos de precios de manera eficiente.
 
 
-
-
-
-
+## Características
+Consulta el precio actual de un producto en una cadena específica en una fecha determinada.
+Proporciona un servicio RESTful para consultar precios.
+Almacena los datos de precios en una base de datos H2 en memoria para una fácil administración y acceso.
+Se integra con Swagger para documentar la API y facilitar su uso.
 
 ## Caso de Uso
 ### Consultar Precio Actual
@@ -25,44 +24,87 @@ Consultar Precio Actual
 Este diagrama de flujo representa el flujo de interacción entre el usuario y el sistema para consultar el precio actual de un producto.
 
 ![](url completa de la imagen)
-## Instalación
 
+## Instalación
 Para ejecutar la aplicación en tu máquina local, sigue estos pasos:
 
 ```bash
-  git clone https://github.com/shaila13/gft-e-commerce-price.git
-  cd gft-e-commerce-price
+  git clone https://github.com/shaila13/ejercicio_ah_shaila_back.git
+  cd ejercicio_ah_shaila_back
   ./mvnw spring-boot:run
 ```
-La aplicación se ejecutará en https://localhost:8080/api/v1.
-## Uso
+La aplicación se ejecutará en http://localhost:8080/swagger-ui/index.html
 
+## Uso
 Una vez que la aplicación esté en funcionamiento, se pueden enviar solicitudes HTTP para consultar precios utilizando el endpoint REST proporcionado.
+
+## Consultar Precio
 Envía una solicitud GET a /prices con los siguientes parámetros de consulta:
 
+brandId: Identificador de la cadena.
+productId: Identificador del producto.
+applicationDate: Fecha de aplicación con formato yyyy-MM-dd HH:mm:ss.
+Ejemplo de solicitud:
+
 ```bash
-GET /prices?applicationDate=2022-01-01T10:00:00&productId=35455&brandId=1
-
+GET /prices?brandId=1&productId=35455&applicationDate=2022-01-01 10:00:00
 ```
-
 
 ## API Reference
 
-#### Get all products
-
+#### Get prices
 ```http
-  GET /api/v1/prices
+  GET /prices
 ```
 
 | Parameter  | Type        | Description                |
 | :--------- | :---------- | :------------------------- |
-| `date`  | `Timestamp` | **Required**. Fecha aplicación con formato yyyy-MM-dd-HH.mm.ss. |
+| `applicationDate`  | `String` | **Required**. Fecha aplicación con formato 'yyyy-MM-dd-HH.mm.ss'. |
 | `productId`| `long` | **Required**. Identificador del producto. |
 | `brandId`  | `long` | **Required**. Identificador de la cadena. |
 
 Por ejemplo:
 
-GET /prices?date=2020-06-15-10.00.00&productId=35455&brandId=1
+GET /prices?brandId=1&productId=35455&applicationDate=2022-01-01 10:00:00
+
+## Respuesta Exitosa
+{
+  "prices": [
+    {
+      "productId": 35455,
+      "brandId": 1,
+      "priceList": 1,
+      "startDate": "2022-01-01T00:00:00",
+      "endDate": "2022-01-01T23:59:59",
+      "price": 35.50
+    },
+    {
+      "productId": 35455,
+      "brandId": 1,
+      "priceList": 2,
+      "startDate": "2022-01-01T15:00:00",
+      "endDate": "2022-01-01T18:30:00",
+      "price": 25.45
+    }
+  ]
+}
+
+## Respuestas de Error
+404 Not Found
+Cuerpo: { "status": "404", "message": "No se encontraron precios para los parámetros proporcionados." }
+400 Bad Request
+Cuerpo: { "status": "400", "message": "Formato de fecha incorrecto. Se esperaba 'yyyy-MM-dd HH:mm:ss'." }
+
+## Construido Con
+Java 17
+Spring Boot
+Maven
+H2 Database
+Swagger
+
+Autores
+Shaila Pérez Fernández - https://github.com/shaila13
+
 ## Licencia
 Este proyecto está bajo la Licencia MIT.
 [MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/test/java/com/shaila/ejercicio/service/PricesInfoUseCaseTest.java
@@ -0,0 +1,111 @@
+package com.shaila.ejercicio.service;
+
+import com.shaila.ejercicio.application.services.PriceService;
+import com.shaila.ejercicio.application.usescases.GetPricesUseCaseImpl;
+import com.shaila.ejercicio.domain.models.Price;
+import com.shaila.ejercicio.domain.ports.out.PriceRepositoryPort;
+import com.shaila.ejercicio.infraestructure.dto.PriceDto;
+import com.shaila.ejercicio.infraestructure.dto.ResponsePriceDto;
+import com.shaila.ejercicio.infraestructure.exception.InvalidParameterException;
+import com.shaila.ejercicio.infraestructure.exception.PriceNotFoundException;
+import com.shaila.ejercicio.infraestructure.repositories.JpaPriceRepositoryAdapter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class PricesInfoUseCaseTest {
+
+	Long brandId = 1L;
+	Long productId = 35455L;
+	String applicationDate = "2020-06-14 00:00:00";
+	String applicationDateWrong = "2020-06-14-00:00:00";
+	Price price ;
+	Price price2 ;
+
+	PriceDto priceDto;
+	PriceDto priceDto2;
+	List<Price> priceList ;
+	List<PriceDto> priceDtoList;
+	ResponsePriceDto responsePriceDto;
+	@MockBean(name = "priceRepositoryPort")
+	PriceRepositoryPort priceRepositoryPort;
+
+	@MockBean(name = "jpaPriceRepositoryAdapter")
+	JpaPriceRepositoryAdapter jpaPriceRepositoryAdapter;
+	@InjectMocks
+	PriceService priceService;
+
+	@InjectMocks
+	GetPricesUseCaseImpl getPricesUseCase;
+
+	@BeforeEach
+	void setUp() {
+		price = new Price(brandId, LocalDateTime.now(), LocalDateTime.now().plusHours(1),
+				1, productId, 1, 35.50, "EUR");
+		price2 = new Price(brandId, LocalDateTime.now(), LocalDateTime.now().plusHours(1),
+				2, productId, 1, 25.45, "EUR");
+
+		priceDto = new PriceDto(brandId,productId,1,  LocalDateTime.now(), LocalDateTime.now().plusHours(1),
+				35.50);
+		priceDto2 = new PriceDto(brandId, productId,2, LocalDateTime.now(), LocalDateTime.now().plusHours(1),
+				  25.45);
+		priceList = Arrays.asList(price,price2);
+		priceDtoList = Arrays.asList(priceDto,priceDto2);
+		responsePriceDto = new ResponsePriceDto(priceDtoList);
+		priceRepositoryPort = mock(PriceRepositoryPort.class);
+		jpaPriceRepositoryAdapter = mock(JpaPriceRepositoryAdapter.class);
+		getPricesUseCase = new GetPricesUseCaseImpl(priceRepositoryPort);
+		priceService = new PriceService(getPricesUseCase);
+	}
+
+	@Test
+	public void shouldReturnPricesWhenCalledWithValidParameters(){
+		when(priceRepositoryPort.findByBrandIdAndProductIdDateApplication(any(),any(),any())).thenReturn(priceList);
+
+		ResponsePriceDto result = priceService.getPricesInfo(brandId, productId, applicationDate);
+		assertNotNull(result);
+		assertEquals(priceList.size(), result.getPrices().size());
+		assertEquals(35.50, result.getPrices().get(0).getPrice());
+
+		IntStream.range(0, priceList.size())
+				.forEach(i -> {
+					Price expectedPrice = priceList.get(i);
+					PriceDto actualPrice = result.getPrices().get(i);
+
+					assertEquals(expectedPrice.getBrandId(), actualPrice.getBrandId());
+					assertEquals(expectedPrice.getProductId(), actualPrice.getProductId());
+					assertEquals(expectedPrice.getPriceList(), actualPrice.getPriceList());
+					assertEquals(expectedPrice.getPrice(), actualPrice.getPrice());
+				});
+	}
+
+	@Test
+	public void shouldReturnPriceNotFoundExceptionWhenDoNotFindAnyPrices() {
+		when(priceRepositoryPort.findByBrandIdAndProductIdDateApplication(anyLong(), anyLong(), any()))
+				.thenReturn(Collections.emptyList());
+		assertThrows(PriceNotFoundException.class,
+				() -> getPricesUseCase.getPricesInfo(brandId, productId, applicationDate));
+	}
+
+	@Test
+	public void shouldReturnInvalidParameterExceptionWhenCalledWithNoValidDates() {
+		when(priceRepositoryPort.findByBrandIdAndProductIdDateApplication(anyLong(), anyLong(), any()))
+				.thenReturn(priceList);
+		assertThrows(InvalidParameterException.class,
+				() -> getPricesUseCase.getPricesInfo(brandId, productId, applicationDateWrong));
+	}
+}

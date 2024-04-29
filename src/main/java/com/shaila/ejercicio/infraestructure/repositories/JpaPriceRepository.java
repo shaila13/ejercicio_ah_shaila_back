3c3a81a50b7ff70451b292830e4fb3adf4e0da1b
Shaila <shailaperezfernandez@gmail.com>
2024-04-29 17:32:49 +0200

    Traducción texto inglés
    Traducción texto inglés

--- src/main/java/com/shaila/ejercicio/infraestructure/repositories/JpaPriceRepository.java
+++ src/main/java/com/shaila/ejercicio/infraestructure/repositories/JpaPriceRepository.java
@@ -8,20 +8,19 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 import java.util.Optional;
 /**
- * Repositorio JPA para acceder a los datos de precios en la base de datos.
- * Proporciona métodos para realizar consultas personalizadas sobre la entidad Prices.
+ * JPA repository to access price data in the database.
+ * Provides methods for performing customized queries on the Prices entity.
  */
 @Repository
 public interface JpaPriceRepository extends JpaRepository<Prices, Long> {
 
     /**
-     * Consulta personalizada para obtener una lista de precios por brandId, productId y fecha de aplicación.
-     * La consulta busca precios que estén vigentes para la fecha de aplicación proporcionada.
-     *
-     * @param brandId        ID de la marca.
-     * @param productId      ID del producto.
-     * @param applicationDate Fecha de aplicación para la cual se buscan los precios.
-     * @return Lista de precios que coinciden con los parámetros proporcionados, ordenados por prioridad descendente.
+     * Custom query to retrieve a single price entry by brandId, productId, and application date,
+     * ordered by descending priority.
+     * @param brandId Brand ID.
+     * @param productId Product ID.
+     * @param applicationDate Application date for which the price is sought.
+     * @return Optional containing the price entry matching the provided parameters.
      */
     @Query("SELECT p FROM Prices p WHERE p.brandId = :brandId AND p.productId = :productId AND :applicationDate between p.startDate and p.endDate ORDER BY p.priority DESC LIMIT 1")
     Optional<Prices> findByBrandIdAndProductIdAndApplicationDateOrderByPriorityDesc
